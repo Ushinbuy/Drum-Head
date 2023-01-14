b22124e7fdce6a25f3dccbf7a199a177ed94dfdb
Ushinbuy <n.s.shadrin@gmail.com>
2023-01-14 22:32:42 +0300

    Minor changes

--- Code/DrumidyF412/USB_DEVICE/App/usbd_desc.c
+++ Code/DrumidyF412/USB_DEVICE/App/usbd_desc.c
@@ -66,7 +66,7 @@
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "STMicroelectronics"
 #define USBD_PID_FS     22336
-#define USBD_PRODUCT_STRING_FS     "Drumidy v2"
+#define USBD_PRODUCT_STRING_FS     "Drumidy v2.0"
 #define USBD_CONFIGURATION_STRING_FS     "Midi Config"
 #define USBD_INTERFACE_STRING_FS     "Midi Interface"
 
