3bc9eb812fb9a0be1c9589113512680096f91187
Ushinbuy <n.s.shadrin@gmail.com>
2023-01-21 15:47:17 +0300

    Add hex description for wave header

--- Code/DrumidyF412/Core/User/Inc/wavFile.h
+++ Code/DrumidyF412/Core/User/Inc/wavFile.h
@@ -3,20 +3,20 @@
 
 typedef struct WAVE_FormatTypeDef
 {
-  uint32_t   ChunkID;       /* 0 */
-  uint32_t   FileSize;      /* 4 */
-  uint32_t   FileFormat;    /* 8 */
-  uint32_t   SubChunk1ID;   /* 12 */
-  uint32_t   SubChunk1Size; /* 16*/
-  uint16_t   AudioFormat;   /* 20 */
-  uint16_t   NbrChannels;   /* 22 */
-  uint32_t   SampleRate;    /* 24 */
+  uint32_t   ChunkID;       /* 0 	0x00 */
+  uint32_t   FileSize;      /* 4 	0x04 */
+  uint32_t   FileFormat;    /* 8 	0x08 */
+  uint32_t   SubChunk1ID;   /* 12 	0x0C */
+  uint32_t   SubChunk1Size; /* 16	0x10 */
+  uint16_t   AudioFormat;   /* 20 	0x14 */
+  uint16_t   NbrChannels;   /* 22 	0x16 */
+  uint32_t   SampleRate;    /* 24 	0x18 */
 
-  uint32_t   ByteRate;      /* 28 */
-  uint16_t   BlockAlign;    /* 32 */
-  uint16_t   BitPerSample;  /* 34 */
-  uint32_t   SubChunk2ID;   /* 36 */
-  uint32_t   SubChunk2Size; /* 40 */
+  uint32_t   ByteRate;      /* 28 	0x1C */
+  uint16_t   BlockAlign;    /* 32 	0x20 */
+  uint16_t   BitPerSample;  /* 34 	0x22 */
+  uint32_t   SubChunk2ID;   /* 36 	0x24 */
+  uint32_t   SubChunk2Size; /* 40 	0x28 */
 
 }WAVE_FormatTypeDef;
 
