460da70cd9ed390f5f240b39fc898ead0b453f8d
Ushinbuy <n.s.shadrin@gmail.com>
2023-02-23 21:16:32 +0400

    Add work with memorySystem

--- Code/TestsDrum/src/memmoryManager.cpp
+++ Code/TestsDrum/src/memmoryManager.cpp
@@ -8,14 +8,18 @@
 
 typedef uint8_t byte;
 
-uint8_t pageAddress[SECTOR_SIZE];
+static uint8_t pageAddress[SECTOR_SIZE];
 #define PADS_NUMBER 6
 #define MAX_SOUNDS 256
 
 static void writeToExternalFlash(uint8_t* pData, uint32_t WriteAddr, uint32_t Size);
-void filFFvalues(uint64_t *pData, uint32_t size);
+static void filFFvalues(uint64_t *pData, uint32_t size);
+static void readMemorySystem(void);
+static void writePadInfo(uint8_t padNum);
+uint32_t calcPadAddress(uint8_t padNum);
 
-struct sPadMemory{
+// this structure do not be used in main project
+struct PadMemory{
 public:
 	  byte sensitivity = 100;   //0
 	  byte threshold1 = 10;     //1
@@ -37,22 +41,31 @@ struct MemorySystem{
 	uint32_t soundsAdresses[MAX_SOUNDS];
 };
 
+static MemorySystem mainMemSyst;
+
 void eraseSector(void) {
+	// this imitate function
 	memset(pageAddress, 0xFF, SECTOR_SIZE);
 }
 
 void writeToExternalFlash(uint8_t* pData, uint32_t WriteAddr, uint32_t Size){
-	// this emulate working of flash memory
+	// change this function for QSPI_write
 	for(uint32_t i = 0; i < Size; i++){
 		pageAddress[WriteAddr + i] &= pData[i];
 	}
 
-	for(int i = 2; i < 100; i++){
-		printf ("\n%3d - 0x%X", i, pageAddress[WriteAddr + i]);
-	}
+//	for(int i = 2; i < 100; i++){
+//		printf ("\n%3d - 0x%X", i, pageAddress[WriteAddr + i]);
+//	}
+}
+
+void readFromExternalFlash(uint8_t* pData, uint32_t ReadAddr, uint32_t Size){
+	// change this function for QSPI_read
+	memcpy(pData, &pageAddress[ReadAddr], Size);
 }
 
 void initMemmorySystem(void){
+	// this imitate function
 	MemorySystem _memSyst;
 	_memSyst.numberOfTotalPads = PADS_NUMBER;
 	_memSyst.soundsNumber = 2;
@@ -69,14 +82,49 @@ void initMemmorySystem(void){
 	writeToExternalFlash((uint8_t *) & _memSyst, ADDRESS_SYSTEM_START, sizeof(MemorySystem));
 }
 
+void writePadInfo(uint8_t padNum){
+	PadMemory pad;
+	pad.curvetype = 2;
+	pad.soundAddressItem = 2;
+
+	writeToExternalFlash((uint8_t*)&pad, calcPadAddress(padNum), sizeof(PadMemory));
+}
+
+uint32_t calcPadAddress(uint8_t padNum){
+	uint64_t offsetInBytes = mainMemSyst.padSavingsOffsets[padNum];
+//	offsetInBytes >>= 10;
+	uint8_t numOfOverwrites = 0;
+	while(offsetInBytes){
+		numOfOverwrites++;
+		offsetInBytes >>= 1;
+	}
+	numOfOverwrites = 64 - numOfOverwrites;
+
+	return ADDRESS_SYSTEM_START + sizeof(MemorySystem) + padNum * sizeof(PadMemory) * numOfOverwrites;
+}
+
 void filFFvalues(uint64_t *pData, uint32_t size){
+	// this imitate function
 	for(uint32_t i = 0; i < size; i++){
 		pData[i] = 0xFFFFFFFFFFFFFFFF;
 	}
+}
+
+void readMemorySystem(void){
+	readFromExternalFlash((uint8_t *)&mainMemSyst, ADDRESS_SYSTEM_START, sizeof(MemorySystem));
 }
 
 void mainWork(void)
 {
 	eraseSector();
 	initMemmorySystem();
+	readMemorySystem();
+
+	// check memory system
+	printf ("CHECK 0x%X", mainMemSyst.soundsAdresses[4]);
+
+	writePadInfo(0);
+	for(int i = calcPadAddress(0); i < calcPadAddress(0) + 11; i++){
+		printf ("\naddress %d has value %d", i, pageAddress[i]);
+	}
 }
