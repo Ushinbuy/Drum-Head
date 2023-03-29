ecd124c371540f7b806e0896b903b54258e27243
Ushinbuy <n.s.shadrin@gmail.com>
2023-03-29 08:30:25 +0300

    Add volatile to audioState and audioBufferOffset

--- Code/F7Drum/Core/User/hellodrum/Src/drumSound.cpp
+++ Code/F7Drum/Core/User/hellodrum/Src/drumSound.cpp
@@ -28,8 +28,8 @@ typedef enum {
 static uint8_t audioBuffer[AUDIO_BUFFER_SIZE] = {0};
 static uint8_t *pBufferFirstHalf = &audioBuffer[0];
 static uint8_t *pBufferSecondHalf = &audioBuffer[AUDIO_BUFFER_SIZE / 2];
-static AUDIO_PLAYBACK_StateTypeDef audioState = AUDIO_STATE_IDLE;
-static BUFFER_StateTypeDef audioBufferOffset = BUFFER_OFFSET_NONE;
+volatile static AUDIO_PLAYBACK_StateTypeDef audioState = AUDIO_STATE_IDLE;
+volatile static BUFFER_StateTypeDef audioBufferOffset = BUFFER_OFFSET_NONE;
 
 DrumSound::DrumSound(uint32_t soundAddress, float soundVolumeDb){
 	WAVE_FormatTypeDef *waveformat = NULL;
